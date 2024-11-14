b1f0214115b764014b3862cd7829fc34ebe8c211
Lou <lourenzo.pro@gmail.com>
2024-11-14 11:31:37 -0300

    [v2] Testing buttons message with image

--- src/api/integrations/channel/meta/whatsapp.business.service.ts
+++ src/api/integrations/channel/meta/whatsapp.business.service.ts
@@ -826,6 +826,18 @@ export class BusinessStartupService extends ChannelStartupService {
               },
             },
           };
+
+          // Adicionar o header condicionalmente, se existir no message
+          if (message['header']) {
+            content.interactive.header = {
+              type: message['header']['type'],
+              [message['header']['type']]: {
+                link: message['header']['link'],
+              },
+            };
+          }
+          // ----------------------------
+
           quoted ? (content.context = { message_id: quoted.id }) : content;
           let formattedText = '';
           for (const item of message['buttons']) {
@@ -1133,6 +1145,16 @@ export class BusinessStartupService extends ChannelStartupService {
             },
           };
         }),
+
+        // Adiciona o header caso venha imagem
+        header: data.mediaType && data.mediaLink ? {
+          type: data.mediaType,
+          [data.mediaType]: {
+            link: data.mediaLink,
+          }
+        } : undefined,
+        // -------------------------------
+
         [embeddedMedia?.mediaKey]: embeddedMedia?.message,
       },
       {
